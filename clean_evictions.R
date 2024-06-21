1d874cfeb2fef56376e1ae13b805cab73e0a10dc
Samantha Toet <samanthactoet@gmail.com>
2024-06-21 13:46:25 -0400

    identify non-person plaintiffs

---
+++ clean_evictions.R
@@ -0,0 +1,37 @@
+
+# Clean eviction data
+library(tidyverse)
+remotes::install_github("virginiaequitycenter/ECtools")
+library(ECtools)
+
+# TODO: 
+# -calculate median principal amount per zip- DONE eda.R
+# -clean and organize judgments - DONE eda.R
+# -defendent plaintiff present - DONE 
+# -non-person plaintiffs 
+
+evictions <- read.delim("data/raw/cases_residential_only.txt", sep = ",")
+
+# Identify if the defendent had an attorney present 
+evictions$defendant_attorney <- na_if(evictions$defendant_attorney, "")
+
+evictions <- evictions %>%
+  mutate(
+    d_attorney_present = case_when(
+      defendant_attorney == "NONE" | defendant_attorney == "SELF-REPRESENTED" | is.na(defendant_attorney) ~ FALSE,
+      TRUE ~ TRUE),
+    principal_amount = parse_number(principal_amount))
+
+# Identify non-residential plaintiffs 
+evictions$plaintiff_non_residential <- identify_non_residential(evictions$plaintiff_name)
+
+# Random spot checks:
+
+# s1 <- sample_n(evictions, 20) %>%
+#   select(plaintiff_name, clean_party_name, plaintiff_non_residential)
+# 
+# s2 <- sample_n(evictions, 20) %>%
+#   select(plaintiff_name, clean_party_name, plaintiff_non_residential)
+# 
+# s3 <- sample_n(evictions, 20) %>%
+#   select(plaintiff_name, clean_party_name, plaintiff_non_residential)
